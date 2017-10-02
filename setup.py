f60f7f28cf75e3bb23054b8475ea4acfb082a2ec
Daniel Hahler <git@thequod.de>
2017-10-02 22:40:40 +0200

    unwrap setup.py

--- setup.py
+++ setup.py
@@ -88,8 +88,15 @@ setup(
     },
     install_requires=REQUIRED,
     extras_require={
-        'testing': ['pytest', 'pytest-catchlog', 'pytest-cov', 'pytest-mock'],
-        'dev': ['pdbpp'],
+        'testing': [
+            'pytest',
+            'pytest-catchlog',
+            'pytest-cov',
+            'pytest-mock',
+        ],
+        'dev': [
+            'pdbpp',
+        ],
     },
     include_package_data=True,
     license='MIT',
